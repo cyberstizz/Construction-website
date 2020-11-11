717c0cbaa8cd572020b14eeb51369a42ded5c24e
cyberstizz <charles.lamb.dev@gmail.com>
2020-11-11 15:06:22 -0500

    made header flex with the direction of row

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import './App.css';
 function App() {
   return (
     <div className="app">
-      <div id="header">
- </div>
-      <div id='images'>
-
-      </div>
+      <div id="header"></div>
+      <div id='images'> </div>
       <div id='bodytop'></div>
       <div id='bodymiddle'></div>
+      <div id='bodybottom'></div>
+      <div id='footertop'></div>
+      <div id='footermiddle'></div>
+      <div id='footerbottom'></div>
+
 
     </div>
    
