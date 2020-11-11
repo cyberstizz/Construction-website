b0656edbb2f93b5da79b813aa0543a4350eea5d9
cyberstizz <charles.lamb.dev@gmail.com>
2020-11-11 16:25:22 -0500

    changed to a class component for state management without hooks for now

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
+class App extends React.Component{
+  constructor(){
+    super()
+  };
+  render(){
+
+
   return (
     <div className="app">
       <div id="header">
@@ -27,7 +34,7 @@ function App() {
 
     </div>
    
-  );
+  )};
 }
 
 export default App;
